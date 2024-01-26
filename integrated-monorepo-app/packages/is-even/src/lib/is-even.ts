a703730572551c65e27c4908b23af130813e39a1
Kevin Huang <khuang@workforcesoftware.com>
2024-01-26 11:29:11 -0800

    first commit

---
+++ integrated-monorepo-app/packages/is-even/src/lib/is-even.ts
@@ -0,0 +1,7 @@
+// import { isOdd } from '@monorepo-app/is-odd';
+import _ from 'lodash';
+
+export function isEven(num: number): string {
+  const stringNumber = _.toString(num);
+  return stringNumber;
+}
